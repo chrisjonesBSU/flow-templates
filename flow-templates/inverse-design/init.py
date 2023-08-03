32de100a8a2d93d47dfaee44096be99cf3cfb40f
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-08-03 16:34:04 -0600

    updating init and project files

---
+++ flow-templates/inverse-design/init.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""Initialize the project's data space.
+
+Iterates over all defined state points and initializes
+the associated job workspace directories.
+The result of running this file is the creation of a signac workspace:
+    - signac.rc file containing the project name
+    - signac_statepoints.json summary for the entire workspace
+    - workspace/ directory that contains a sub-directory of every individual statepoint
+    - signac_statepoints.json within each individual statepoint sub-directory.
+
+"""
+
+import signac
+import flow
+import logging
+from collections import OrderedDict
+from itertools import product
+
+
+def get_parameters():
+    ''''''
+    # System Details:
+    parameters = OrderedDict()
+    parameters["bead_sequence"] = [["A"]]
+    parameters["chain_lengths"] = [20]
+    parameters["num_mols"] = [20]
+    parameters["bead_mass"] = [{"A": 100}]
+    parameters["bond_lengths"] = [{"A-A": 1.0}]
+    parameters["density"] = [1.2]
+    parameters["ref_length"] = [dict(value=1, units="nm"]
+    parameters["ref_energy"] = [dict(value=1, units="kJ"]
+    parameters["ref_mass"] = [dict(value=450, units="amu"]
+    
+    # Forcefield Details:
+    parameters["pair_forces"] = [{"A": {"sigma": 1.0, "epsilon": 1.0}}]
+    parameters["bond_forces"] = [{"A-A": {"r0": 1.0, "k": 500}}]
+    parameters["angle_forces"] = [{"A-A-A": {"t0": 1.0, "k": 100}}]
+    parameters["dihedral_forces"] = [
+            {"A-A-A-A": {"phi0": 1.0, "k": 100, "n": 1, "d": -1}}
+    ]
+    parameters["r_cut"] = [2.5]
+    parameters["exclusions"] = ["bond", "1-3"]
+
+    # Simulation Details:
+    parameters["kT"] = [5.0]
+    parameters["n_steps"] = [5e8]
+    parameters["shrink_kT"] = [8.0]
+    parameters["shrink_n_steps"] = [5e7]
+    parameters["shrink_period"] = [10000]
+    parameters["dt"] = [0.0003]
+    parameters["tau_kT"] = [100]
+    parameters["gsd_write_freq"] = [1e4]
+    parameters["log_write_freq"] = [1e3]
+    return list(parameters.keys()), list(product(*parameters.values()))
+
+
+def main():
+    project = signac.init_project() # Set the signac project name
+    param_names, param_combinations = get_parameters()
+    # Create the generate jobs
+    for params in param_combinations:
+        statepoint = dict(zip(param_names, params))
+        job = project.open_job(statepoint)
+        job.init()
+        job.doc.setdefault("sim_done", False)
+        job.doc.setdefault("sample_done", False)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
---
+++ flow-templates/inverse-design/project.py
@@ -0,0 +1,203 @@
+"""Define the project's workflow logic and operation functions.
+
+Execute this script directly from the command line, to view your project's
+status, execute operations and submit them to a cluster. See also:
+
+    $ python src/project.py --help
+"""
+import signac
+from flow import FlowProject, directives
+from flow.environment import DefaultSlurmEnvironment
+import os
+
+
+class MyProject(FlowProject):
+    pass
+
+
+class Borah(DefaultSlurmEnvironment):
+    hostname_pattern = "borah"
+    template = "borah.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="shortgpu",
+            help="Specify the partition to submit to."
+        )
+
+
+class R2(DefaultSlurmEnvironment):
+    hostname_pattern = "r2"
+    template = "r2.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="shortgpuq",
+            help="Specify the partition to submit to."
+        )
+
+
+class Fry(DefaultSlurmEnvironment):
+    hostname_pattern = "fry"
+    template = "fry.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="batch",
+            help="Specify the partition to submit to."
+        )
+
+# Definition of project-related labels (classification)
+@MyProject.label
+def sim_done(job):
+    return job.doc.sim_done
+
+
+@MyProject.label
+def sample_done(job):
+    return job.doc.sample_done
+
+
+def build_system(job):
+    lj_chains = LJChain(
+            n_mols=job.sp.n_mols,
+            lengths=job.sp.chain_lengths,
+            bead_sequence=job.sp.bead_sequence,
+            bead_mass=job.sp.bead_mass,
+            bond_lengths=job.sp.bond_lengths
+    )
+
+    system = Pack(
+            molecules=lj_chains,
+            density=job.sp.density,
+            r_cut=job.sp.r_cut,
+            packing_expand_factor=job.sp.packing_expand_factor,
+            edge=job.sp.packing_edge,
+            overlap=job.sp.packing_overlap,
+    )
+
+    length_units = getattr(unyt, job.sp.ref_length["units"])
+    energy_units = getattr(unyt, job.sp.ref_energy["units"])
+    mass_units = getattr(unyt, job.sp.ref_mass["units"])
+    ref_length = job.sp.ref_length["value"] * length_units
+    ref_energy = job.sp.ref_length["value"] * energy_units
+    ref_mass = job.sp.ref_length["value"] * mass_units
+
+    job.doc.ref_length = ref_length
+    job.doc.ref_energy = ref_energy
+    job.doc.ref_mass = ref_mass
+    system.reference_length = ref_length
+    system.reference_energy = ref_ref_energy
+    system.reference_mass = ref_mass
+
+	# Set up Forcefield:
+	beads = dict()
+	bonds = dict()
+	angles = dict()
+	dihedrals = dict()
+
+	for bead in job.sp.bead_types[0]:
+		beads[bead] = job.sp.bead_types[0][bead]
+
+	for bond in job.sp.bond_types[0]:
+		bonds[bond] = job.sp.bond_types[0][bond]
+
+	for angle in job.sp.angle_types[0]:
+		angles[angle] = job.sp.angle_types[0][angle]
+
+	for dih in job.sp.dihedral_types[0]:
+		dihedrals[dih] = job.sp.dihedral_types[0][dih]
+
+	bead_spring_ff = BeadSpring(
+			beads=beads,
+			bonds=bonds,
+			angles=angles,
+			dihedrals=dihedrals,
+			r_cut=job.sp.r_cut
+	)
+
+    hoomd_ff = bead_spring_ff.hoomd_forcefield
+    snapshot = system.hoomd_snapshot
+    ref_units = (system.ref_distance, system.ref_energy, system.ref_mass)
+    return snapshot, hoomd_ff, ref_units
+
+
+@MyProject.post(sim_done)
+@MyProject.operation(
+        directives={"ngpu": 1, "executable": "python -u"}, name="simulation"
+)
+def run_sim(job):
+    import hoomd_polymers
+    from hoomd_polymers.base.system import Pack
+    from hoomd_polymers.base.simulation import Simulaton 
+    with job:
+        snapshot, hoomd_ff, ref_units = build_system(job) 
+        gsd_path = job.fn("trajectory.gsd")
+        log_path = job.fn("log.txt")
+        sim = Simulation(
+                initial_state=snapshot,
+                forcefield=hoomd_ff,
+                dt=job.sp.dt,
+                r_cut=job.sp.r_cut,
+                gsd_write_freq=job.sp.gsd_write_freq,
+                gsd_file_name=gsd_path,
+                log_write_freq=job.sp.log_write_freq,
+                log_file_name=log_path,
+        )
+        sim.pickle_forcefield(job.fn("forcefield.pickle"))
+        sim.reference_length = ref_units[0] 
+        sim.reference_energy =  ref_units[1] 
+        sim.reference_mass = ref_units[2] 
+        # Store unit information in job doc
+        tau_kT = sim.dt * job.sp.tau_kT 
+        job.doc.tau_kT = tau_kT
+        job.doc.target_box = target_box
+        job.doc.real_time_step = sim.real_timestep.to("fs").value()
+        job.doc.real_time_units = "fs"
+        job.doc.n_steps = job.sp.n_steps
+        job.doc.simulation_time = job.doc.real_time_step * job.doc.n_steps
+        job.doc.shrink_time = job.doc.real_time_step * job.sp.shrink_n_steps
+        # Set up stuff for shrinking volume step 
+        print("Running shrink step.")
+        target_box = system.target_box / system.reference_distance.to("angstrom").value()
+        shrink_kT_ramp = sim.kT_ramp(
+                n_steps=job.sp.shrink_n_steps,
+                kT_start=job.sp.shrink_kT,
+                kT_final=job.sp.kT
+        )
+        sim.run_update_volume(
+                final_box=target_box,
+                n_steps=job.sp.shrink_n_steps,
+                period=job.sp.shrink_period,
+                tau_kt=tau_kT,
+                kT=shrink_kT_ramp
+        )
+        print("Shrink step finished.")
+        print("Running simulation.")
+        sim.run_NVT(kT=job.sp.kT, n_steps=job.sp.n_steps, tau_kt=tau_kT)
+        sim.save_restart_gsd(job.fn("restart.gsd"))
+        print("Simulation finished.")
+
+
+@MyProject.pre(sim_done)
+@MyProject.post(sample_done)
+@MyProject.operation(
+        directives={"ngpu": 1, "executable": "python -u"}, name="sample"
+)
+def sample(job):
+    # Add package imports here
+    with job:
+        print("JOB ID NUMBER:")
+        print(job.id)
+        print("------------------------------------")
+        # Add your script here
+
+
+if __name__ == "__main__":
+    MyProject().main()
