53af0beecd4f6530f8c2758098fbbb826799107a
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-10-12 16:12:56 -0600

    update to use some of the new api stuff

--- flow-templates/flowermd/project.py
+++ flow-templates/flowermd/project.py
@@ -55,8 +55,8 @@ class Fry(DefaultSlurmEnvironment):
 
 # Definition of project-related labels (classification)
 @MyProject.label
-def sim_done(job):
-    return job.doc.sim_done
+def nvt_done(job):
+    return job.doc.nvt_done
 
 
 @MyProject.label
@@ -64,11 +64,11 @@ def sample_done(job):
     return job.doc.sample_done
 
 
-@MyProject.post(sim_done)
+@MyProject.post(nvt_done)
 @MyProject.operation(
-        directives={"ngpu": 1, "executable": "python -u"}, name="simulation"
+        directives={"ngpu": 1, "executable": "python -u"}, name="nvt"
 )
-def run_sim(job):
+def run_nvt(job):
     import flowermd
     from flowermd.base.system import Pack
     from flowermd.base.simulation import Simulaton
@@ -86,56 +86,63 @@ def run_sim(job):
                     )
             mol_obj_list.append(mol_obj)
 
+        ff_obj_list = []
+        for ff in job.sp.forcefields:
+            force_obj = getattr(flowermd.library.forcefields, ff)
+            ff_obj_list.append(force_obj())
+
         system = Pack(
-                    molecules=mol_obj_list,
-                    density=job.sp.density,
-                    r_cut=job.sp.r_cut,
-                    auto_scale=True,
-                    remove_hydrogens=job.sp.remove_hydrogens,
-                    remove_charges=job.sp.remove_charges
-                )
+                molecules=mol_obj_list,
+                density=job.sp.density,
+                r_cut=job.sp.r_cut,
+                auto_scale=True,
+                remove_hydrogens=job.sp.remove_hydrogens,
+                remove_charges=job.sp.remove_charges
+        )
+
+        system.apply_forcefield(
+                force_field=ff_obj_list,
+                r_cut=job.sp.r_cut,
+                auto_scale=job.sp.auto_scale,
+                scale_charges=True,
+                remove_charges=job.sp.remove_charges,
+                remove_hydrogens=job.sp.remove_hydrogens,
+                pppm_resolution=job.sp.pppm_resolution,
+                pppm_order=job.sp.pppm_order
+        )
 
         gsd_path = job.fn("trajectory.gsd")
         log_path = job.fn("log.txt")
-        sim = Simulation(
-                initial_state=system.hoomd_snapshot,
-                forcefield=system.hoomd_forcefield,
+
+        sim = Simulation.from_system(
+                system=system,
                 dt=job.sp.dt,
-                r_cut=job.sp.r_cut,
                 gsd_write_freq=job.sp.gsd_write_freq,
                 gsd_file_name=gsd_path,
                 log_write_freq=job.sp.log_write_freq,
                 log_file_name=log_path,
         )
         sim.pickle_forcefield(job.fn("forcefield.pickle"))
-        sim.reference_length = system.reference_length
-        sim.reference_energy = system.reference_energy
-        sim.reference_mass = system.reference_mass
         # Store unit information in job doc
         tau_kT = sim.dt * job.sp.tau_kT
         job.doc.tau_kT = tau_kT
-        job.doc.target_box = target_box
         job.doc.ref_mass = sim.reference_mass.to("amu").value()
         job.doc.ref_mass_units = "amu"
         job.doc.ref_energy = sim.reference_energy.to("kJ/mol").value()
         job.doc.ref_energy_units = "kJ/mol"
-        job.doc.ref_length = sim.reference_length.to("angstrom").value()
-        job.doc.ref_length_units = "angstrom"
+        job.doc.ref_length = sim.reference_length.to("nm").value()
+        job.doc.ref_length_units = "nm"
         job.doc.real_time_step = sim.real_timestep.to("fs").value()
         job.doc.real_time_units = "fs"
-        job.doc.n_steps = job.sp.n_steps
-        job.doc.simulation_time = job.doc.real_time_step * job.doc.n_steps
-        job.doc.shrink_time = job.doc.real_time_step * job.sp.shrink_n_steps
         # Set up stuff for shrinking volume step
         print("Running shrink step.")
-        target_box = system.target_box / system.reference_distance.to("angstrom").value()
         shrink_kT_ramp = sim.kT_ramp(
                 n_steps=job.sp.shrink_n_steps,
                 kT_start=job.sp.shrink_kT,
                 kT_final=job.sp.kT
         )
         sim.run_update_volume(
-                final_box=target_box,
+                final_density=job.sp.density,
                 n_steps=job.sp.shrink_n_steps,
                 period=job.sp.shrink_period,
                 tau_kt=tau_kT,
@@ -145,10 +152,11 @@ def run_sim(job):
         print("Running simulation.")
         sim.run_NVT(kT=job.sp.kT, n_steps=job.sp.n_steps, tau_kt=tau_kT)
         sim.save_restart_gsd(job.fn("restart.gsd"))
+        job.doc.nvt_done = True
         print("Simulation finished.")
 
 
-@MyProject.pre(sim_done)
+@MyProject.pre(nvt_done)
 @MyProject.post(sample_done)
 @MyProject.operation(
         directives={"ngpu": 1, "executable": "python -u"}, name="sample"
@@ -160,6 +168,7 @@ def sample(job):
         print(job.id)
         print("------------------------------------")
         # Add your script here
+        job.doc.sample_done = True
 
 
 if __name__ == "__main__":
